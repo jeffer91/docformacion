58e2899ee1c1062b9e0f97961692e4b9feb57bd7
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-08-31 10:13:37 -0500

    feat: include editable careers and document codes in Excel template

--- main.js
+++ main.js
@@ -104,8 +104,34 @@ ipcMain.handle('excel:template', async () => {
     const periodHeaders = [[
       'PERIODO_INICIO', 'PERIODO_FIN', 'FECHA_ELABORACION', 'VERSION',
       'ELABORADO_POR', 'CARGO_ELABORADO', 'REVISADO_POR', 'CARGO_REVISADO',
-      'APROBADO_POR', 'CARGO_APROBADO', 'META_FORMACION_PORCENTAJE'
+      'APROBADO_POR', 'CARGO_APROBADO', 'META_FORMACION_PORCENTAJE',
+      'CODIGO_DNF', 'CODIGO_PLAN', 'CODIGO_INFORME'
     ]];
+
+    const careerRows = [
+      ['CARRERA','PROGRAMA'],
+      ['Enfermería','Técnico Superior'],
+      ['Mecánica Automotriz','Tecnología Superior'],
+      ['Diseño Multimedia','Tecnología Superior'],
+      ['Marketing Digital y Comercio Electrónico','Tecnología Superior'],
+      ['Ventas','Tecnología Superior'],
+      ['Desarrollo de Software','Tecnología Superior'],
+      ['Desarrollo de Software y Ciberseguridad','Tecnología Universitaria'],
+      ['Redes y Telecomunicaciones','Tecnología Superior'],
+      ['Estética Integral','Tecnología Superior'],
+      ['Educación Básica','Tecnología Superior'],
+      ['Educación Inicial','Tecnología Superior'],
+      ['Pedagogía','Tecnología Universitaria'],
+      ['Procesamiento de Alimentos','Tecnología Superior'],
+      ['Administración','Tecnología Superior'],
+      ['Administración de Empresas e inteligencia de negocios','Tecnología Universitaria'],
+      ['Administración del Talento Humano','Tecnología Universitaria'],
+      ['Contabilidad','Tecnología Superior'],
+      ['Contabilidad y Tributación','Tecnología Universitaria'],
+      ['Gestión del Talento Humano','Tecnología Superior'],
+      ['Seguridad y Prevención de Riesgos Laborales','Tecnología Superior'],
+      ['Seguridad Ciudadana y Orden Publico','Tecnología Superior']
+    ];
 
     const teacherHeaders = [[
       'CEDULA', 'NOMBRE_COMPLETO', 'CARRERA_PRINCIPAL', 'DEDICACION',
@@ -117,7 +143,7 @@ ipcMain.handle('excel:template', async () => {
     ]];
 
     const coordHeaders = [[
-      'CARRERA', 'COORDINADOR', 'PRIORIDAD_MANUAL'
+      'CARRERA', 'COORDINADOR', 'NECESIDADES', 'PRIORIDAD_MANUAL'
     ]];
 
     const planHeaders = [[
@@ -132,6 +158,7 @@ ipcMain.handle('excel:template', async () => {
       'PORCENTAJE_AVANCE', 'TITULO_EVIDENCIA', 'RUTA_EVIDENCIA', 'ABANDONO'
     ]];
 
+    XLSX.utils.book_append_sheet(wb, XLSX.utils.aoa_to_sheet(careerRows), 'CARRERAS');
     XLSX.utils.book_append_sheet(wb, XLSX.utils.aoa_to_sheet(periodHeaders), 'PERIODO');
     XLSX.utils.book_append_sheet(wb, XLSX.utils.aoa_to_sheet(teacherHeaders), 'DOCENTES');
     XLSX.utils.book_append_sheet(wb, XLSX.utils.aoa_to_sheet(coordHeaders), 'COORDINACIONES');
