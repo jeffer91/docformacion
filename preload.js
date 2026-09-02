895768c1148a6ce8f996c8bb295d2cf6a5a082ab
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-02 10:31:48 -0500

    feat: pass scoped Excel template payloads

--- preload.js
+++ preload.js
@@ -4,7 +4,7 @@ contextBridge.exposeInMainWorld('docformacion', {
   loadData: () => ipcRenderer.invoke('data:load'),
   saveData: (data) => ipcRenderer.invoke('data:save', data),
   importExcel: () => ipcRenderer.invoke('excel:import'),
-  exportExcelTemplate: () => ipcRenderer.invoke('excel:template'),
+  exportExcelTemplate: (payload) => ipcRenderer.invoke('excel:template', payload),
   pickEvidence: () => ipcRenderer.invoke('evidence:pick'),
   generatePDF: (payload) => ipcRenderer.invoke('pdf:generate', payload),
   readFirebase: () => ipcRenderer.invoke('firebase:read')
