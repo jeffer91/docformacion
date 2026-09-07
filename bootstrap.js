b59620c6c2e9e1dd460e39b165b18e2915ec6648
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-07 12:40:41 -0500

    chore: load DNF quality document engine

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
 (() => {
-  const LOCAL_BUILD = '20260907-1215';
+  const LOCAL_BUILD = '20260907-1230';
   const isHttp = location.protocol === 'http:' || location.protocol === 'https:';
 
   function setBuildLabel(build) {
@@ -50,6 +50,7 @@
     await loadScript('app.js?v=' + encodeURIComponent(activeBuild));
     await loadScript('period-dnf-fix.js?v=' + encodeURIComponent(activeBuild));
     await loadScript('institutional-plan-fix.js?v=' + encodeURIComponent(activeBuild));
+    await loadScript('dnf-quality-fix.js?v=' + encodeURIComponent(activeBuild));
   }
 
   start().catch(error => {
