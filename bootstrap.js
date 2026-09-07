451d08354802005ff7e8a1431e5c74fdd9109351
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-07 15:36:02 -0500

    chore: publish stronger DNF PDF recovery build

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
 (() => {
-  const LOCAL_BUILD = '20260907-1500';
+  const LOCAL_BUILD = '20260907-1525';
   const isHttp = location.protocol === 'http:' || location.protocol === 'https:';
 
   function setBuildLabel(build) {
