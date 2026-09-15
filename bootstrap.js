1d3c4708874d535e263cc66618197fe2029bdef1
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-15 11:27:08 -0500

    chore: bump build after DNF persistence fix

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
 (() => {
-  const LOCAL_BUILD = '20260915-1020';
+  const LOCAL_BUILD = '20260915-1128';
   const isHttp = location.protocol === 'http:' || location.protocol === 'https:';
 
   function setBuildLabel(build) {
