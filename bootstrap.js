9cd52ee3805712851f336c347b2ddb269842f189
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-17 10:40:26 -0500

    chore: bump build for responsive direct previews

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
 (() => {
-  const LOCAL_BUILD = '20260917-1000';
+  const LOCAL_BUILD = '20260917-1015';
   const isHttp = location.protocol === 'http:' || location.protocol === 'https:';
 
   function setBuildLabel(build) {
